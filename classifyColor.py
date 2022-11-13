48eb99c01364455aa4e307e192ae6759922a4673
Grzetan <grzetan@gmail.com>
2022-11-13 12:58:57 +0100

    Added HSV color space as option to classyfing colors

--- classifyColor.py
+++ classifyColor.py
@@ -1,6 +1,7 @@
 import argparse
 import ast
 import math
+import colorsys
 
 ap = argparse.ArgumentParser()
 
@@ -24,8 +25,8 @@ ap.add_argument(
 
 ap.add_argument(
   "--max_dist",
-  type=int,
-  default=20
+  type=float,
+  default=0.2
 )
 
 args = vars(ap.parse_args())
@@ -45,6 +46,10 @@ with open(args['template_colors']) as f:
         template_colors.append(template_color)
 
 def distBetweenColors(col1, col2):
+    if args['method'] == 'hsv':
+        col1 = colorsys.rgb_to_hsv(col1[0]/255.0, col1[1]/255.0, col1[2]/255.0)
+        col2 = colorsys.rgb_to_hsv(col2[0]/255.0, col2[1]/255.0, col2[2]/255.0)
+
     return math.sqrt((col1[0]-col2[0])**2 + (col1[1]-col2[1])**2 + (col1[2]-col2[2])**2)
 
 closest_color = 0
@@ -55,7 +60,13 @@ for i, template_color in enumerate(template_colors):
         closest_color = i
         closest_dist = dist
 
-if closest_dist > args['max_dist']:
+max_dist = 0
+if args['method'] == 'rgb':
+    max_dist = args['max_dist'] * math.sqrt(195075) # Max distance in RGB space
+elif args['method'] == 'hsv':
+    max_dist = args['max_dist'] * math.sqrt(3) # Max distance in HSV space
+
+if closest_dist > max_dist:
     print("Input color doesn't classify to any of template colors")
     exit()
 
